67ebe2b39eb9b2e75c7648883a653702af31ef07
alextitonis <alexandrostitonis@gmail.com>
2022-06-01 19:01:57 +0300

    Initial Commit

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+public class Server : MonoBehaviour
+{
+    public static Server getInstance;
+    void Awake() { getInstance = this; }
+
+    [SerializeField] int port;
+    [SerializeField] [Tooltip("Set to -1 to disable")] int maxClients = -1;
+    [SerializeField] string connectionKey;
+
+    EventBasedNetListener listener;
+    NetManager server;
+
+    void Start()
+    {
+        listener = new EventBasedNetListener();
+        server = new NetManager(listener);
+        server.Start(port);
+        listener.ConnectionRequestEvent += ConnectionRequestEvent;
+        listener.PeerConnectedEvent += PeerConnectedEvent;
+        listener.NetworkReceiveEvent += NetworkReceiveEvent;
+    }
+
+    void Update()
+    {
+        if (server != null)
+            server.PollEvents();
+    }
+
+    void NetworkReceiveEvent(NetPeer peer, NetPacketReader reader, byte channel, DeliveryMethod deliveryMethod)
+    {
+        Packets packet = (Packets)reader.GetUShort();
+        
+    }
+
+    void PeerConnectedEvent(NetPeer peer)
+    {
+        NetDataWriter writer = new NetDataWriter();
+        writer.Put((ushort)Packets.Welcome);
+        writer.Put(peer.Id);
+        SendPacket(writer, peer, DeliveryMethod.ReliableOrdered);
+    }
+
+    void ConnectionRequestEvent(ConnectionRequest request)
+    {
+        if (maxClients == 0)
+            request.Reject();
+        else if (maxClients < 0)
+            request.AcceptIfKey(connectionKey);
+    }
+
+    void SendPacket(NetDataWriter writer, NetPeer peer, DeliveryMethod method)
+    {
+        peer.Send(writer, method);
+    }
+
+}
+
+public enum Packets
+{
+    Welcome = 0,
+}
